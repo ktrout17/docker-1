5f1d37fd1858a5dbc1c3c621409f43613aa76700
Kristin Trout <ktrout@student.wethinkcode.co.za>
2020-12-14 21:41:23 +0200

    added app submodule

--- 01_dockerfiles/ex01/run.sh
+++ 01_dockerfiles/ex01/run.sh
@@ -4,7 +4,7 @@ echo -e "\e[96mStarting teamspeak..\e[0m"
 docker run -d --name teamspeak --rm -p 9987:9987/udp -p 10011:10011 -p 30033:30033 ex01
 echo -e "\e[92mTeamspeak now running..\e[0m"
 
-echo -e "\e[92mConnect with local client to ip of local docker machine:"
-echo -e "do \'docker logs teamspeak\' to find port of virtual server and privilege key" 
-echo -e "open teamspeak and connect to $(docker-machine ip Char):<port from log> and enter key when prompted\e[0m"
-echo -e "\e[92mWhen finished, run \`docker stop teamspeak\`\e[0m"
+echo -e "\e[93mConnect with local client to ip of local docker machine:"
+echo -e "\e[93mdo \'docker logs teamspeak\' to find port of virtual server and get privilege key" 
+echo -e "\e[93mopen teamspeak and connect to $(docker-machine ip Char):<port from log> and enter key when prompted\e[0m"
+echo -e "\e[93mWhen finished, run \`docker stop teamspeak\`\e[0m"
