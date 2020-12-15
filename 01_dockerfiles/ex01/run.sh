99851ba5a5d836c5e6947347484b47e8b50f91af
Kristin Trout <ktrout@student.wethinkcode.co.za>
2020-12-15 11:44:29 +0200

    small touchups - COMPLETE

--- 01_dockerfiles/ex01/run.sh
+++ 01_dockerfiles/ex01/run.sh
@@ -9,6 +9,6 @@ docker run -d --name teamspeak --rm -p 9987:9987/udp -p 10011:10011 -p 30033:300
 echo -e "\e[92mTeamspeak now running..\e[0m"
 
 echo -e "\e[93mConnect with local client to ip of local docker machine:"
-echo -e "\e[93mdo \'docker logs teamspeak\' to find port of virtual server and get privilege key" 
+echo -e "\e[93mdo \`docker logs teamspeak\` to find port of virtual server and get privilege key" 
 echo -e "\e[93mopen teamspeak and connect to $(docker-machine ip Char):<port from log> and enter key when prompted\e[0m"
 echo -e "\e[93mWhen finished, run \`docker stop teamspeak\`\e[0m"
